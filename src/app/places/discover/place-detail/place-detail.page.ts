915173e171f5b68350e8973218524809281fba4f
davuthumainr <davut@humainr.com>
2022-05-31 15:03:58 +0200

    backward

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; //angular routing
+import { NavController } from '@ionic/angular'; //ionic routing
 
 @Component({
   selector: 'app-place-detail',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+  constructor(private router: Router, private navController: NavController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onBookPlace() {
+    // this.router.navigateByUrl('/places/tabs/discover');//navigation angular
+    this.navController.navigateBack('/places/tabs/discover');//navigation ionic
+    // this.navController.pop();//that will not work after page refreshing.
   }
-
 }
