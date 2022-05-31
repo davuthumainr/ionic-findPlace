8a171120563448d4d62f7d2fe63bdc1f6de50579
davuthumainr <davut@humainr.com>
2022-05-31 10:04:05 +0200

    adjustment has been made in the places.service.ts

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class PlacesService {
+  //places array
+  private _places = [];
+
+  //get copy of places with getter method
+  get places() {
+    return [...this._places];
+  }
 
   constructor() {}
 }
