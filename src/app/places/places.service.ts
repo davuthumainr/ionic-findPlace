a65b8dd5456a60a64409f0e700539c3aa6b3f605
davuthumainr <davut@humainr.com>
2022-05-31 10:59:00 +0200

    added Place class and a few places

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Place } from './places.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
   //places array
-  private _places = [];
+  private _places: Place[] = [
+    new Place(
+      'p1',
+      'Amsterdam',
+      'Capital of Netherlands',
+      'http://www.sporcle.com/blog/wp-content/uploads/2018/02/1-14.jpg',
+      139.99
+    ),
+    new Place(
+      'p2',
+      'Roterdam',
+      `Europe's port to the world.`,
+      'https://www.ubm-development.com/magazin/wp-content/uploads/2020/12/S_top_PowerhouseCompany_Codrico00_Plomp.jpg',
+      129.99
+    ),
+    new Place(
+      'p3',
+      'Groningen Agoda',
+      'A beautiful historic city',
+      'https://www.27vakantiedagen.nl/wp-content/uploads/2021/02/groningen-stad.jpg',
+      129.99
+    ),
+  ];
 
   //get copy of places with getter method
   get places() {
