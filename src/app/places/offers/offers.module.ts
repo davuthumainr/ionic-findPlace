3daf96ea4cf592cd113856e374158b56981847ee
davuthumainr <davut@humainr.com>
2022-06-09 11:50:09 +0200

    adding custom CSS rules

--- src/app/places/offers/offers.module.ts
+++ src/app/places/offers/offers.module.ts
@@ -7,14 +7,18 @@ import { IonicModule } from '@ionic/angular';
 import { OffersPageRoutingModule } from './offers-routing.module';
 
 import { OffersPage } from './offers.page';
+import { OfferItemComponent } from './offer-item/offer-item.component';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: OffersPage,
+  },
+];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    OffersPageRoutingModule
-  ],
-  declarations: [OffersPage]
+  imports: [CommonModule, FormsModule, IonicModule, OffersPageRoutingModule],
+  declarations: [OffersPage, OfferItemComponent],
 })
 export class OffersPageModule {}
