e59da7d0c87695e6a7aa5c57127a132555dabf3a
davuthumainr <davut@humainr.com>
2022-06-13 11:27:06 +0200

    finishing the offer form template

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-offer.page.scss'],
 })
 export class NewOfferPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  //
+  onCreateOffer() {
+    console.log('Creating offered place...');
   }
-
 }
