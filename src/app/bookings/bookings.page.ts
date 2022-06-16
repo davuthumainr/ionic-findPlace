58d44c752b3dd576d4ab15d6c8aa4ee0229da6db
davuthumainr <davut@humainr.com>
2022-06-16 13:53:26 +0200

    cancelling bookings

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Booking } from './bookings.model';
 import { BookingsService } from './bookings.service';
-import { IonItemSliding } from '@ionic/angular';
+import { IonItemSliding, LoadingController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -13,7 +13,10 @@ export class BookingsPage implements OnInit, OnDestroy {
   loagedBookings: Booking[];
   private bookingSub: Subscription;
 
-  constructor(private bookingsService: BookingsService) {}
+  constructor(
+    private bookingsService: BookingsService,
+    private loadingControlling: LoadingController
+  ) {}
 
   ngOnDestroy() {
     if (this.bookingSub) {
@@ -27,8 +30,17 @@ export class BookingsPage implements OnInit, OnDestroy {
     });
   }
 
-  onCancelBooking(offerId: string, slidingBookingItemToLeft: IonItemSliding) {
+  onCancelBooking(bookingId: string, slidingBookingItemToLeft: IonItemSliding) {
     slidingBookingItemToLeft.close();
-    //cancel booking with id offerId
+    this.loadingControlling
+      .create({
+        message: 'Cancelling...',
+      })
+      .then((loadingElement) => {
+        loadingElement.present();
+        this.bookingsService.cancelBooking(bookingId).subscribe(() => {
+          loadingElement.dismiss();
+        });
+      });
   }
 }
